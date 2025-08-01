ad6251059cca0e7bd3e3c3f94a588f2176582250
franklyn sierra <franklynssierracontreras@gmail.com>
2025-08-01 12:17:47 -0400

    fdg

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './login.css'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 export default function Login({setAuth,setUser}) {
